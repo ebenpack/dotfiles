b0b4ec4c5b09125760ac4b67c90aebb3a0523a9c
ebenpack <ebenpackwood@gmail.com>
2013-09-13 13:26:18 -0400

    First commit

---
+++ qtile/config.py
@@ -0,0 +1,191 @@
+from libqtile.config import Key, Screen, Group, Click, Drag
+from libqtile.command import lazy
+from libqtile import layout, bar, widget, hook
+
+mod = "mod4"
+
+widget_defaults = dict(
+    border_focus="#de4377",
+    border_normal="#ad8e99"
+)
+
+keys = [
+    # Log out
+    Key([mod, "shift"], "q",
+        lazy.shutdown()),
+    Key([mod, "shift"], "r",
+        lazy.restart()),
+    Key([mod, "shift"], "c",
+        lazy.window.kill()),
+
+    # Switch between windows in current stack pane
+    Key([mod], "l",
+        lazy.layout.down()),
+    Key([mod], "h",
+        lazy.layout.up()),
+
+    # Resize windows in current stack pane
+    Key([mod, "shift"], "h", 
+        lazy.layout.decrease_ratio()),
+    Key([mod, "shift"], "l", 
+        lazy.layout.increase_ratio()),
+
+    # MonadTall resize
+    ####
+    # It would be nice if these key bindings could be same as the two above. Is this possible?
+    ####
+    Key([mod, "shift"], "j", 
+        lazy.layout.grow()),
+    Key([mod, "shift"], "k", 
+	lazy.layout.shrink()),
+
+    # Move windows up or down in current stack
+    Key([mod, "control"], "k",
+        lazy.layout.shuffle_down()),
+    Key([mod, "control"], "j",
+        lazy.layout.shuffle_up()),
+
+    # Switch window focus to other pane(s) of stack
+    Key([mod], "space",
+        lazy.layout.next()),
+
+    # Swap panes of split stack
+    Key([mod, "shift"], "space",
+        lazy.layout.rotate()),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with multiple stack panes
+    Key([mod, "shift"], "Return",
+        lazy.layout.toggle_split()),
+    # Key([mod], "h",
+    #     lazy.to_screen(1)),
+    # Key([mod], "l",
+    #     lazy.to_screen(0)),
+    
+    # Launch specific applications
+    Key([mod], "Return", 
+        lazy.spawn("terminator")),
+    Key([mod], "w",
+        lazy.spawn("firefox")),
+    Key([mod], "r",
+        lazy.spawncmd()),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "space",
+        lazy.nextlayout()),
+    Key([mod], "f",
+        lazy.window.toggle_fullscreen()),
+    Key([mod, "control"], "space",
+        lazy.window.toggle_floating()),
+    Key([mod], "m",
+        lazy.window.toggle_maximize()),
+    Key([mod], "n",
+        lazy.window.toggle_minimize()),
+
+
+    
+    # Move between groups
+    
+    Key([mod], "Right",
+        lazy.screen.nextgroup()),
+    Key([mod], "Left",
+        lazy.screen.prevgroup()),
+
+]
+
+mouse = [
+   Drag([mod], "Button1", lazy.window.set_position_floating(),
+       start=lazy.window.get_position()),
+   Drag([mod], "Button3", lazy.window.set_size_floating(),
+       start=lazy.window.get_size()),
+   Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+groups = [
+    Group("1"),
+    Group("2"),
+    Group("3"),
+    Group("4"),
+    Group("5"),
+    Group("6"),
+    Group("7"),
+    Group("8"),
+]
+for i in groups:
+    # mod4 + letter of group = switch to group
+    keys.append(
+        Key([mod], i.name, lazy.group[i.name].toscreen()))
+
+    # mod4 + shift + letter of group = switch to & move focused window to group
+    keys.append(
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)))
+
+dbgroups_key_binder = None
+dbgroups_app_rules = []
+
+layouts = [
+    layout.Tile(**widget_defaults),
+    layout.RatioTile(**widget_defaults),
+    layout.MonadTall(**widget_defaults),
+    layout.Max(),
+    layout.TreeTab()
+]
+
+screens = [
+    Screen(
+        top = bar.Bar(
+                    [
+                        widget.GroupBox(
+                            urgent_alert_method='text',
+                            fontsize=10,
+                            borderwidth=1),
+                        widget.CurrentLayout(),
+                        widget.WindowName(foreground = "a0a0a0",),
+                        widget.Prompt(),
+                        widget.Notify(),
+                        widget.Systray(),
+                        widget.Wlan(interface="wlp4s0"),
+                        widget.Battery(
+                            energy_now_file='energy_now',
+                            energy_full_file='energy_full',
+                            power_now_file='energy_now',
+                            update_delay = 5,
+                            foreground = "7070ff",), 
+                        widget.Volume(foreground = "70ff70"),
+                        widget.Clock(foreground = "a0a0a0",
+                            fmt = '%Y-%m-%d %a %I:%M %p'),
+                    ],
+                    22,
+                ),
+    ),
+]
+
+import subprocess, re
+
+def is_running(process):
+    s = subprocess.Popen(["ps", "axuw"], stdout=subprocess.PIPE)
+    for x in s.stdout:
+        if re.search(process, x):
+            return True
+    return False
+
+def execute_once(process, options=""):
+    if not is_running(process):
+        process = process.split()
+        if options:
+            process.extend(options.split())
+        return subprocess.Popen(process)
+
+main = None
+follow_mouse_focus = True
+cursor_warp = False
+floating_layout = layout.Floating()
+mouse = ()
+
+@hook.subscribe.startup
+def startup():
+    execute_once("conky")
+    execute_once("dropboxd")
+    #execute_once("clipit")
+    execute_once("redshift", "-l 44.6:-68.37 -t 5700:3600 -g 0.8 -m randr")
