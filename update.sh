c19cc1732141bd2b896318347c62c7fd60ffad99
ebenpack <ebenpackwood@gmail.com>
2013-09-13 15:52:32 -0400

    2013-09-13 update

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DATE=`date +%Y-%m-%d`
+DATE=`date +%Y-%m-%d\ %H:%M`
 
-yaourt -Q > $HOME/.dotfiles/pkg.list
+yaourt -Qe > $HOME/.dotfiles/pkg.list
 git --git-dir $HOME/.dotfiles/.git/ --work-tree=$HOME/.dotfiles add -A $HOME/.dotfiles/
-git --git-dir $HOME/.dotfiles/.git/ --work-tree=$HOME/.dotfiles commit -a -m "$DATE Update"
+git --git-dir $HOME/.dotfiles/.git/ --work-tree=$HOME/.dotfiles commit -a -m "$DATE update"
 git --git-dir $HOME/.dotfiles/.git/ status
 git --git-dir $HOME/.dotfiles/.git/ --work-tree=$HOME/.dotfiles push -u origin master
-#yaourt -Syua
+yaourt -Syua
